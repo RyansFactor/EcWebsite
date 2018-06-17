c4f251275a42ccab0966640f1bde486d6bb46575
sago <sago@localhost>
2018-06-17 22:19:34 +0900

    ツールなおした

--- ec-20180610/view/tool_view.php
+++ ec-20180610/view/tool_view.php
@@ -11,60 +11,67 @@
 
 <body class="toolView">
 
-	<h1>商品管理画面</h1>
+<h1>商品管理画面</h1>
 <?php print $result_msg; ?>
 <form method="POST" enctype="multipart/form-data">
-		<input type="hidden" name="sql_kind" value="itemInsert">
-		<table class="toolTable">
-			<tr class="toolTableBack">
-				<th>商品画像1</th>
-				<th>商品画像2</th>
-				<th>商品名</th>
-				<th>価格</th>
-				<th>サイズ</th>
-				<th>在庫数</th>
-				<th>色</th>
-				<th>ステータス</th>
-				<th>処理ボタン</th>
-			</tr>
+	<input type="hidden" name="sql_kind" value="itemInsert">
+	<table class="toolTable">
+		<tr class="toolTableBack">
+			<th>商品画像1</th>
+			<th>商品画像2</th>
+			<th>商品名</th>
+			<th>価格</th>
+			<th>サイズ</th>
+			<th>在庫数</th>
+			<th>色</th>
+			<th>ステータス</th>
+			<th>処理ボタン</th>
+		</tr>
 
-			<tr>
-				<td><input type="file" name="img1"></td>
-				<td><input type="file" name="img2"></td>
-				<td><input type="text" name="name" value=""></td>
-				<td><input type="number" name="price" value=""></td>
-				<td><select name="size">
-						<option value="S">S</option>
-						<option value="M">M</option>
-						<option value="L">L</option>
-						<option value="XL">XL</option>
-				</select></td>
-				<td><input type="number" name="stock" value=""></td>
-				<td><select name="color">
-						<option value="Black">Black</option>
-						<option value="Gray">Gray</option>
-						<option value="Red">Red</option>
-						<option value="Pink">Pink</option>
-						<option value="Orange">Orange</option>
-						<option value="Yellow">Yellow</option>
-						<option value="Green">Green</option>
-						<option value="Lightblue">Lightblue</option>
-						<option value="blue">blue</option>
-						<option value="white">white</option>
-				</select></td>
-				<td><select name="status">
-						<option value="1">公開</option>
-						<option value="0">非公開</option>
-				</select></td>
-				<td><input type="submit" value="商品を追加"></td>
-			</tr>
-			<tr>
-				<th class="toolTableBack">コメント</th>
-				<td colspan="8"><textarea name="comment" rows="10" cols="120"
-						value=""></textarea></td>
-			</tr>
-		</table>
+		<tr>
+			<td><input type="file" name="img1"></td>
+			<td><input type="file" name="img2"></td>
+			<td><input type="text" name="name" value=""></td>
+			<td><input type="number" name="price" value=""></td>
+			<td>
+				<select name="size">
+    				<option value="S">S</option>
+    				<option value="M">M</option>
+    				<option value="L">L</option>
+    				<option value="XL">XL</option>
+				</select>
+			</td>
+			<td><input type="number" name="stock" value=""></td>
+			<td>
+				<select name="color">
+					<option value="Black">Black</option>
+					<option value="Gray">Gray</option>
+					<option value="Red">Red</option>
+					<option value="Pink">Pink</option>
+					<option value="Orange">Orange</option>
+					<option value="Yellow">Yellow</option>
+					<option value="Green">Green</option>
+					<option value="Lightblue">Lightblue</option>
+					<option value="blue">blue</option>
+					<option value="white">white</option>
+				</select>
+			</td>
+			<td>
+				<select name="status">
+					<option value="1">公開</option>
+					<option value="0">非公開</option>
+				</select>
+			</td>
+			<td><input type="submit" value="商品を追加"></td>
+		</tr>
+		<tr>
+			<th class="toolTableBack">コメント</th>
+			<td colspan="8"><textarea name="comment" rows="10" cols="120" value=""></textarea></td>
+		</tr>
+	</table>
+</form>
 <hr>
+
 <table>
 <?php
 
